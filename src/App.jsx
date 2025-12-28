46cf09a514c3c7e347e5ad45805063fb80488813
dipesh-cwd <codewithdipesh44@gmail.com>
2025-12-28 15:27:18 +0545

    initiall commit, now all folders are setuped and ready to learn animations in react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Simple} from './components/index.js'
+const App = () => {
+  return (
+    <div>
+      <Simple />
+      
+    </div>
+  )
+}
+
+export default App
